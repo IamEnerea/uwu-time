f6a9a3c2dc9610607ae232d9a142ddbf6591ced0
IamEnerea <enerea512@gmail.com>
2026-01-25 17:53:27 -0500

    connect bot to discord

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("☕🎀 Uwu Time iniciado correctamente");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`☕🎀 Uwu Time está online como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
